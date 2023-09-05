751dd4789c69dfaf6c93add674eef62c0486cdf1
parthiv8848 <124236867+parthiv8848@users.noreply.github.com>
2023-09-06 00:59:22 +0530

    Update Login.js

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
 
 
     const Handlelogin = async () => {
-        let result = await fetch('https://e-dashboarddone.vercel.app/login', {
+        let result = await fetch('https://e-dashboarddone-git-main-parthiv8848.vercel.app/login', {
             method: 'post',
             body: JSON.stringify({ email, password }),
             headers: {
