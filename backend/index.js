d3efbf8699c46277a7549f8131d5075eb88d9af2
parthiv8848 <124236867+parthiv8848@users.noreply.github.com>
2023-09-08 01:50:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -132,17 +132,13 @@ app.post('/add-product', upload.single('image'), async (req, res) => {
     // Write the processed image to the server
     fs.writeFileSync(imagePath, imageBuffer);
 
-    // Define the base URL where your images are hosted
-    const imageBaseUrl = 'https://okdone.onrender.com';
-
     // Create a new product with the processed image path
     const newProduct = new Product({
       name,
       price,
       category,
       company,
-      // Prepend the imageBaseUrl to the image path
-     image: `${imageBaseUrl}/${uniqueFileName}`,
+      image: imagePath,
     });
 
     // Save the product to the database
@@ -154,7 +150,6 @@ app.post('/add-product', upload.single('image'), async (req, res) => {
     res.status(500).json({ error: 'Failed to add product' });
   }
 });
-
 
 app.get("/products" ,async (req, resp) => {
     const products = await Product.find();
